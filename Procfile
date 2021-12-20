f692ffcc3294ad5bd794984e26922fab5273389b
“Alex <alexandriadelaney923@gmail.com>
2021-12-20 09:44:15 -0800

    added env and gitignore

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn labinventory.wsgi
+release: python manage.py migrate
--- labinventory/settings.py
+++ labinventory/settings.py
@@ -11,19 +11,31 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kwj#=j%5)3%h)=q2xebz==y7j&9)d0ziz7geb0evlz8-@=pe(z'
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
