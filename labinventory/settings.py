6dbb6840cd801cdf64e42e3825d7423a3620ad79
“Alex <alexandriadelaney923@gmail.com>
2021-12-21 09:19:18 -0800

    moved info to settings.py

--- labinventory/settings.py
+++ labinventory/settings.py
@@ -30,11 +30,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-kwj#=j%5)3%h)=q2xebz==y7j&9)d0ziz7geb0evlz8-@=pe(z'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['*']
